f1ed020a51bf7437ac6adfbc7f37a7658ccdc547
diana <mafka487@gmail.com>
2023-07-09 21:54:45 -0700

    started working on register page

---
+++ src/views/RegisterPage.vue
@@ -0,0 +1,35 @@
+<template> 
+<div class="theWrapper">
+  <input v-model="email" placeholder="Email">
+  <input v-model="name" placeholder="Name - how other people will know you">
+  <input v-model="nickname" placeholder="Nickname - how people will find you">
+  <input v-model="password" placeholder="Password">
+  <input v-model="passwordRepeat" placeholder="Repeat Password">
+  <a href="linkToLoginView">Already have an account?</a>
+</div>
+</template>
+<script setup>
+import { ref } from 'vue';
+import { useCookies } from "vue3-cookies";
+
+function register() {
+    if( password.value !== passwordRepeat ) {
+      //show error component
+      console.log("Error!");
+      return;
+    }
+    fetch('http://localhost/api/user/register', {
+      method: 'POST',
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+          email: email.value,
+          name: name.value,
+          nickname: nickname.value,
+          password: password.value,
+      })
+    })
+}
+</script>
+
